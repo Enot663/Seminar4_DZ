aff76b7e19bfe6d3fb0e17beef4cf76243f148e9
Andrey <andrejsatykov@yandex.ru>
2022-12-07 20:12:27 +0300

    The sum of digits in a number Task 002

---
+++ Task_001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+Console.WriteLine("Для формулы A в степени B");
+Console.Write("Введите число A: ");
+int EnterNumberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int EnterNumberB = int.Parse(Console.ReadLine());
+
+int result = EnterNumberA;
+
+for (int i = 2; i <= EnterNumberB; i++)
+{
+    result *= EnterNumberA;
+}
+
+Console.WriteLine($"Число {EnterNumberA} в {EnterNumberB} степени равно {result}");
---
+++ Task_002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+Console.Write("Введите своё число: ");
+int EnterNumber = int.Parse(Console.ReadLine());
+
+int result = 0;
+
+while (EnterNumber != 0)
+{
+    result += EnterNumber % 10;
+    EnterNumber /= 10;
+}
+
+Console.WriteLine($"Сумма цифр числа равна {result}.");
