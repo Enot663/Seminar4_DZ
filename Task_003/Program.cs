2850ee637158421bfa67c78d2d96eb2d0852a59b
Andrey <andrejsatykov@yandex.ru>
2022-12-07 20:16:19 +0300

    An array of 8 elements task 003

---
+++ Task_003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+Console.Clear();
+
+Console.Write("Введите число элементов массива: "); // 
+int EnterNumber = int.Parse(Console.ReadLine());
+
+int[] array = new int[EnterNumber];
+
+
+for (int i = 0; i < EnterNumber; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i] + "; ");
+}
+
